f61453b4656b3fdafc85c9810096770f62107355
3evils <51053456+3evils@users.noreply.github.com>
2020-05-18 11:21:42 -0400

    upd

---
+++ cache/api_keys.php
@@ -0,0 +1,12 @@
+<?php
+/**
+This file created on May 28 2019 19:42:38.
+Api Key mod by Antimidas and Tundracanine based on Site Settings mod by stoner with a little help from pdq for U-232.
+ **/
+$INSTALLER09['omdb_key'] = '60ac17f0';
+$INSTALLER09['tmdb_key'] = '6e7090479fbbec08eea8d9571e253b38';
+$INSTALLER09['tvdb_key'] = '96c2c8b0f3054c88be8634db268abebf';
+$INSTALLER09['omdb_on'] = '1';
+$INSTALLER09['tmdb_on'] = '1';
+$INSTALLER09['tvdb_on'] = '1';
+?>
